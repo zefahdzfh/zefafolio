d425e30f6e654a08843c473bd51485d25b35f875
zefahdzfh <zefakalibata@gmail.com>
2023-08-31 14:08:09 +0700

    discord update

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbyLCNk02d2a_ai9Oza-sJ
                 })
                 .catch(error => console.error('Error!', error.message))
         })
+
+        
+        // const mainMenu = document.querySelector('.mainMenu');
+        // const closeMenu = document.querySelector('.closeMenu');
+        // const openMenu = document.querySelector('.openMenu');
+        // const menu_items = document.querySelectorAll('nav .mainMenu li a');
+        
+        
+        
+        
+        // openMenu.addEventListener('click',show);
+        // closeMenu.addEventListener('click',close);
+        
+        // // close menu when you click on a menu item 
+        // menu_items.forEach(item => {
+        //     item.addEventListener('click',function(){
+        //         close();
+        //     })
+        // })
+        
+        // function show(){
+        //     mainMenu.style.display = 'flex';
+        //     mainMenu.style.top = '0';
+        // }
+        // function close(){
+        //     mainMenu.style.top = '-100%';
+        // }
